d3ff6b282d0e361e7b864424fda471d345291738
williansa <58983761+williansa@users.noreply.github.com>
2021-03-08 11:53:43 -0300

    aula4

--- src/app/pages/articles/articles.page.ts
+++ src/app/pages/articles/articles.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 // 1) Importa dependências
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,7 +12,10 @@ import { Observable } from 'rxjs';
 export class ArticlesPage implements OnInit {
 
   // 3) Atributos
-  item: Observable<any[]>;
+  items: Observable<any[]>;
+
+  // 3.1) Atributos
+  private itemsCollection: AngularFirestoreCollection<any>;
 
   constructor(
 
@@ -21,7 +24,27 @@ export class ArticlesPage implements OnInit {
   ) {
 
     // 4) Ler dados do banco de dados
-    this.item = firestore.collection('articles').valueChanges({ idField: 'id' });
+    // this.item = firestore.collection('articles').valueChanges({ idField: 'id' });
+
+    // 4.1) Ler dados do banco de dados com filtro
+
+    this.itemsCollection  = firestore.collection<any>(
+      'articles', // Coleção a ser consultada
+      ref => ref // Aplica filtros
+        .where('status', '==', 'ativo') // Somente com 'status'='ativo'
+        .orderBy('date', 'desc') // Ordena por 'date' na ordem decrescente
+
+      /*
+        ATENÇÃO!
+          Será necessário gerar um índice no Firestore para que esta query funcione.
+          O link para gerar o índice aparece no console.
+          Logue-se no Firebase.com e clique no link do console.
+      */
+    );
+
+    // 5. Obtém os ítens da coleção e também o 'id' de cada item.
+    this.items = this.itemsCollection.valueChanges({ idField: 'id' });
+
   }
 
   ngOnInit() { }
--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1) Importa dependências
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { DatePipe } from '@angular/common';
+
+// 6) Não permite somente espaços nos campos
+export function removeSpaces(control: AbstractControl) {
+  if (control && control.value && !control.value.replace(/\s/g, '').length) {
+    control.setValue('');
+  }
+  return null;
+}
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.page.html',
   styleUrls: ['./contacts.page.scss'],
 })
 export class ContactsPage implements OnInit {
+  // 3) Atributos
+  public contactForm: FormGroup; // Contém o formulário de contatos
+  public pipe = new DatePipe('en_US'); // Formatar as datas
 
-  constructor() { }
+  constructor(
+    // 2) Injeta dependências
+    public form: FormBuilder,
+    public firestore: AngularFirestore
+  ) {}
 
   ngOnInit() {
+    // 4) Cria o formulário de contatos
+    this.contactFormCreate();
   }
 
+  // 5) Cria o formulário de contatos
+  contactFormCreate() {
+    this.contactForm = this.form.group({
+      date: [''], // Data de envio
+      name: [
+        // Nome
+        '',
+        Validators.compose([
+          Validators.required, // Obrigatório
+          Validators.minLength(3), // Nomes curtos demais
+          removeSpaces
+        ]),
+      ],
+      email: [
+        // E-mail
+        '',
+        Validators.compose([Validators.required, Validators.email, removeSpaces]),
+      ],
+      subject: [
+        // Assunto
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5), removeSpaces]),
+      ],
+      message: [
+        // Mensagem
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5), removeSpaces]),
+      ],
+    });
+  }
 }
